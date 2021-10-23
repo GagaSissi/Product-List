c06b7c0fd95c930fbbd6f0e1ceac01865fcf5e7b
Cecilia Strüber <strueber.cecilia@googlemail.com>
2021-10-23 17:48:14 +0200

    adding some thoughts

---
+++ script.js
@@ -0,0 +1,19 @@
+// Create constant 1-3, product 1-3, one click.
+const product1Element = [];
+document.getElementById("btn1").addEventListener("click", addProduct1ToCart);
+const product2Element = [];
+document.getElementById("btn2").addEventListener("click", addProduct2ToCart);
+const product2Element = [];
+document.getElementById("btn3").addEventListener("click", addProduct3ToCart);
+
+// class ProductDatas(){
+//     constructor(countProduct1, countProduct2, countProduct3) {
+//         this.produ1 = countProduct1
+//         this.produ2 = countProduct2
+//         this.produ3 = countProduct3
+//     }
+// }
+
+// function addProduct1ToCart() {
+
+// }
